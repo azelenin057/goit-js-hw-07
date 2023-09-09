b735c59384b2c7f1b7e44cd47e1d56f12625468d
azelenin057 <yazel057@gmail.com>
2023-09-09 18:23:42 +0300

    v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return galleryItems
+    .map(
+      ({ description, original, preview }) =>
+        `
+    <li class="gallery__item">
+    <img src="${preview}" alt="${description}" class="gallery__image">
+    </li>
+  `
+    )
+    .join("");
+}
